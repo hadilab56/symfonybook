9c7844c43ec91cd7fb1e9ac681556e0f37407f12
AymericJak <aymericjakpro@gmail.com>
2023-01-22 17:08:56 +0100

    Contraintes form user

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -11,18 +11,31 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
             ->add('firstName', TextType::class, [
                 'label' => 'Votre prénom',
+                'constraints' => new Length(
+                    min: 2,
+                    max: 50,
+                    minMessage: 'Votre prénom doit avoir au minimum {{ limit }} caractères',
+                    maxMessage: 'Votre prénom doit avoir au maximum {{ limit }} caractères',
+                ),
                 'attr' => [
                     'placeholder' => 'Prénom',
                 ],
             ])
             ->add('lastName', TextType::class, [
                 'label' => 'Votre nom',
+                'constraints' => new Length(
+                    min: 2,
+                    max: 50,
+                    minMessage: 'Votre nom doit avoir au minimum {{ limit }} caractères',
+                    maxMessage: 'Votre nom doit avoir au maximum {{ limit }} caractères',
+                ),
                 'attr' => [
                     'placeholder' => 'Nom',
                 ],
@@ -35,10 +48,19 @@ class RegisterType extends AbstractType {
             ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
+                'constraints' => new Length(
+                    min: 8,
+                    minMessage: 'Votre mot de passe doit avoir au minimum {{ limit }} caractères',
+                ),
                 'invalid_message' => 'Le mot de passe et la confirmation doivent être identiques !',
                 'required' => true,
-                'first_options' => ['label' => 'Votre mot de passe'],
-                'second_options' => ['label' => 'Confirmation du mot de passe']
+                'first_options' => ['label' => 'Votre mot de passe',
+                    'attr' => ['placeholder' => "Mot de passe"]
+                    ],
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe',
+                    'attr' => ['placeholder' => "Confirmation du mot de passe"]
+                ]
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'S\'inscrire',
